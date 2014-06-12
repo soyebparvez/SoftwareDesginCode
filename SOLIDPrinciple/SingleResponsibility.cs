565aa21dc30d2186d2fd88095799ea24da48ea39
soyebparvez <soyeb.parvez@gmail.com>
2014-06-13 00:13:37 +0530

    Added code for singleresponsibility

---
+++ SOLIDPrinciple/SingleResponsibility.cs
@@ -0,0 +1,51 @@
+﻿namespace SOLIDPrinciple
+{
+  public class Employee
+  {
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string ID { get; set; }
+    public double GetSalary()
+    {
+      double basic = 10000.00;
+      double hra = 5000.00;
+      double others = 10000;
+
+      return basic + hra + others;
+    }
+  }
+
+  public interface IExtendedEmployee
+  {
+    string Name { get; set; }
+    string Address { get; set; }
+    string ID { get; set; }
+  }
+
+  public class ExtendedEmplyoee : IExtendedEmployee
+  {
+    #region Implementation of IExtendedEmplyee
+
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string ID { get; set; }
+
+    #endregion
+  }
+
+
+  public class EmployeeSalaryCalculator
+  {
+    public double CalculateEmployeeSalary(IExtendedEmployee extendedEmplyee)
+    {
+      // Here some other class or data base give salary component for
+      // against particular employee id.
+
+      double basic = 10000.00;
+      double hra = 5000.00;
+      double others = 10000;
+
+      return basic + hra + others;
+    }
+  }
+}
