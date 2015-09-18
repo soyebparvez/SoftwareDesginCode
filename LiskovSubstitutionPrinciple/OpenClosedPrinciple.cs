e0a6d04067b024d0ff17dafe046d58ee827e02ce
soyebparvez <soyeb.parvez@gmail.com>
2015-09-18 11:06:31 +0530

    Added OCP and DIP
    Added OCP and DIP

---
+++ LiskovSubstitutionPrinciple/OpenClosedPrinciple.cs
@@ -0,0 +1,67 @@
+﻿namespace LiskovSubstitutionPrinciple
+{
+  using System;
+
+  public class PistonEngine
+  {
+  }
+
+  public class Car
+  {
+    public Car()
+    {
+      this.Engine = new PistonEngine();
+    }
+
+    public PistonEngine Engine { get; private set; }
+  }
+
+  public class CarExecuter
+  {
+    public static void Main(string[] args)
+    {
+      Car car = new Car();
+      Console.WriteLine("Car has " + car.Engine.GetType().Name);
+      Console.WriteLine("What happen when we want to change car Engine. we have to modify car class");
+      Console.WriteLine("This design is not close for modification");
+      Console.WriteLine("Now we invert dependency in extended car");
+      Console.ReadKey();
+    }
+  }
+
+  public interface IEngine
+  {
+  }
+
+  public class ExtendedPistonEngine : IEngine
+  {
+  }
+
+  public class ExtendedJetEngine : IEngine
+  {
+  }
+
+  public class ExtendedCar
+  {
+    public ExtendedCar(IEngine engine)
+    {
+      this.Engine = engine;
+    }
+
+    public IEngine Engine { get; private set; }
+  }
+
+  public class ExtendedCarExecuter
+  {
+    public static void Main(string[] args)
+    {
+      ExtendedCar car = new ExtendedCar(new ExtendedPistonEngine());
+      Console.WriteLine("Car has " + car.Engine.GetType().Name);
+
+      ExtendedCar car2 = new ExtendedCar(new ExtendedJetEngine());
+      Console.WriteLine("Car has " + car2.Engine.GetType().Name);
+
+      Console.ReadKey();
+    }
+  }
+}
